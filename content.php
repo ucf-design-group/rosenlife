39db9b78b0b218d915b33ecfe36a062552e48002
chris-judge <chris_judge941@yahoo.com>
2014-09-08 16:26:28 -0400

    Remove title.

--- content.php
+++ content.php
@@ -8,7 +8,6 @@
 
 ?>
 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-						<h1 class="entry-title"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h1>
 
 
 <!-- POST CONTENT START -->
