44553f6b80242a5878a81e58e7ee0fdcf17bd716
chris-judge <chris_judge941@yahoo.com>
2014-09-09 11:31:57 -0400

    Create splash image container along with text container.

--- home.php
+++ home.php
@@ -2,7 +2,14 @@
 <?php get_header(); ?>
 
 			<div class="content-area">
-				<img src="http://placekitten.com/1920/800" alt="">
+				<div class="splashwrap">
+					<div class="splashimage">
+						<div class="innertext">
+							<h1>Kitten Ipsum sister oscar birdwatch!</h1>
+							<p>Kitten Ipsum sister, local oscar birdwatch hiss meowlly her friend bat cat rip. Smile, pillow grandparents aggressive sandi cuddles. Kitty, happy boots impressed hiding ham cat little rip browsing.</p>
+						</div>
+					</div>
+				</div>
 				<div class="main"> 
 					<?php
 					while (have_posts()) {
