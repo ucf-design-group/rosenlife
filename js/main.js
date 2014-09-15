b6a3b29a2663461e98723bf2bcbf29f18b5bbe42
chris-judge <chris_judge941@yahoo.com>
2014-09-15 11:36:21 -0400

    Create sliding animation for event template tiles.

--- js/main.js
+++ js/main.js
@@ -37,6 +37,36 @@ $(document).ready(function () {
 		$("nav.main-menu ul").slideToggle();
 		evt.preventDefault();
 	})
+
+// ---------- EVENT PAGE TILES ---------- //
+
+	var largeUp = false;
+
+	$('.moredetails').mouseleave(function(){
+			if(largeUp == true){
+				$(this).animate({width: "100px"});
+				$(this).removeClass('contentdetails--open');
+				$(this).children('.moredetailstitle').html('<i class="fa fa-chevron-left"></i>');
+				largeUp = false;
+			}
+	});
+
+	$('.moredetails').click(function(){
+		if(largeUp == false){
+			$(this).animate({width: "78%"});
+			$(this).addClass('contentdetails--open');
+			$(this).children('.moredetailstitle').html('<i class="fa fa-chevron-right"></i>');
+			largeUp = true;
+		} else if(largeUp == true){
+			$(this).animate({width: "100px"});
+			$(this).removeClass('contentdetails--open');
+			$(this).children('.moredetailstitle').html('<i class="fa fa-chevron-left"></i>');
+			largeUp = false;
+		}
+	});
+
+
+
 });
 
 
