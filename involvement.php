40e0ee5274584be352d330366177fee9ed93a5f8
chris-judge <chris_judge941@yahoo.com>
2014-09-17 14:15:43 -0400

    Create involvement template.

---
+++ involvement.php
@@ -0,0 +1,17 @@
+<?php /* Template Name: Involvement */ ?>
+<?php get_header(); ?>
+
+			<div class="content-area">
+				<div class="main"> 
+					<?php
+					while (have_posts()) {
+						the_post();
+						get_template_part( 'content' );
+					} ?>
+				</div>
+				<!-- <aside> -->
+					<!-- OPTIONAL -->
+				<!-- </aside> -->
+			</div>
+
+<?php get_footer(); ?>
